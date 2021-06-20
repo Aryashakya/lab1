ab6ba607f9a5ed6dfd71e1150fd8574821a02c55
Arya Shakya <ryaashakya@gmail.com>
2021-06-20 18:07:12 +0545

    Minor changes

--- src/StackQueueMain.cpp
+++ src/StackQueueMain.cpp
@@ -1,9 +1,29 @@
 #include <iostream>
 #include "LinkedList.h"
 #include "Queue.h"
+#include "Stack.h"
 
 int main()
 {
+    LinkedListStack s;
+    try
+    {
+        s.push(1);
+        s.push(2);
+        s.push(3);
+        s.push(4);
+        s.traverse();
+        std::cout << s.top() << std::endl;
+        std::cout << s.pop() << std::endl;
+        std::cout << s.top() << std::endl;
+        std::cout << s.pop() << std::endl;
+        s.traverse();
+    }
+    catch (const char *message)
+    {
+        std::cout << message << '\n';
+    }
+
     LinkedListQueue q;
     try
     {
